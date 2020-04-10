aad0a653b164c7ad8646930d2463054183ca7af4
Ingemar <jahmagnus@gmail.com>
2020-04-10 19:38:54 +0100

    Added functions for hours and minutes

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-import requests
-import json
-import webbrowser
-import pprint
+from datetime import datetime
 import time
+from tkinter import *
 
-run = True
-while run:
-    r = requests.get('http://worldtimeapi.org/api/timezone/Europe/London')
 
-    dateTime = (r.json()['datetime'])
+def getTime():
+    currentDate = datetime.now()
+
+    return currentDate
+
+
+def formatHour():
+    currentDate = getTime()
+    currentHour = str(currentDate.hour).zfill(2)
+
+    return currentHour
+
+
+def formatMinute():
+    currentDate = getTime()
+    currentMinute = str(currentDate.minute).zfill(2)
+
+    return currentMinute
+
+
+def renderImage():
+    currentHour = formatHour()
+    currentMinute = formatMinute()
+
+    hourFirstColumn = int(currentHour[0])
+    hourSecondColumn = int(currentHour[1])
+    minuteFirstColumn = int(currentMinute[0])
+    minuteSecondColumn = int(currentMinute[1])
+
+    print(hourFirstColumn, hourSecondColumn, minuteFirstColumn, minuteSecondColumn)
+
+
+def getHourColumns():
+    currentHour = formatHour()
+
+
+def getMinuteColumns():
+    currentMinute = formatMinute()
+
+
+flag = True
+window = Tk()
+img = PhotoImage(file='simpsons.jpg')
+
+imgLbl = Label(window, image=img)
+while flag:
+    renderImage()
     time.sleep(5)
-
-    print(dateTime)
-
-
-
-
-
-
-
